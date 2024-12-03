c5b5a7485ac5254dcf60fa2a758b9b27192f03c6
isaias-x <daiaisaias13@gmail.com>
2024-12-03 11:33:16 -0300

    add variado
    03.12

---
+++ Aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('Sagrado profano')
+
+nome_usuario = 'Isaias Venancio' # tipo -> str = string
+idade_usuario = 19 # doce22 
+altura_usuario = 1.85 # tipo -> float = ponto flutante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Sejam Bem vinde', nome_usuario,)
+print ('sua idade é' , idade_usuario, 'anos' )
+print ('você tem', altura_usuario, 'metros')
+print ('você é maior de idade?', maior_idade)
